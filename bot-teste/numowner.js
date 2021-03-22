0a43452cb808fcebec1dd7fc5f4da7da5086e714
Hudson613 <zaga921ghudson@gmail.com>
2021-03-22 20:00:31 -0300

    First commit

---
+++ bot-teste/numowner.js
@@ -0,0 +1,12 @@
+const ownerNumber = ["553189517334@s.whatsapp.net"]
+const OriginalOwner = '553189517334'
+const vcard = 'BEGIN:VCARD\n' 
+            + 'VERSION:3.0\n' 
+            + 'FN:Meu criador^~^\n' 
+            + 'ORG:Ian;\n' 
+            + 'TEL;type=CELL;type=VOICE;waid=557187645787:+55 71 8764-5787\n' 
+            + 'END:VCARD'
+
+exports.vcard = vcard
+exports.OriginalOwner = OriginalOwner
+exports.ownerNumber = ownerNumber
